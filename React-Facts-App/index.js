2cf3286cbd7f376d73d4073f7620068b5ac4b2d0
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-13 21:59:58 +0530

    Project Part 1 - Solved

--- React-Facts-App/index.js
+++ React-Facts-App/index.js
@@ -12,3 +12,19 @@ Hints:
 * You can also set the `width` attribute of the image element
   just like in HTML. In the slide, I have it set to 40px
  */
+
+const Content = (
+    <div>
+        <img src="react-logo.png" width="100px"></img>
+        <h1>Fun Facts about React!</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(Content, document.getElementById("root"))
