f50f5cd8de691e89cafb097292ae80dc6e47972c
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-15 14:50:33 +0530

    Parent/Child Components

--- react-facts-app/src/index.js
+++ react-facts-app/src/index.js
@@ -2,22 +2,43 @@ import React from "react"
 import ReactDOM from "react-dom"
 import logo from "./react-logo.png"
 
-function Page() {
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src={logo} alt="React-Logo" width="40px"/>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src={logo} alt="React-Logo" width="40px"/>
-                </nav>
-            </header>
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
                 <li>I'm more likely to get a job as a developer if I know React</li>
             </ol>
-            <footer>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
                 <small>© 2022 iGN1T0R development. All rights reserved.</small>
-            </footer>
+        </footer>
+    )
+}
+
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     )
 }
