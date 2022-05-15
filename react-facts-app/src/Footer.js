e6f73dfa3d7d797a046ec607dd587bb398b6543f
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-15 20:28:30 +0530

    Project Setup

--- react-facts-app/src/Footer.js
+++
@@ -1,9 +0,0 @@
-import "./style.css"
-
-export default function Footer() {
-    return (
-        <footer>
-                <small>© 2022 iGN1T0R development. All rights reserved.</small>
-        </footer>
-    )
-}
