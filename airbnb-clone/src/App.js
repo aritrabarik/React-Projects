96379ad85883e43311000cf0228776cfc4c52f6b
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-16 14:28:37 +0530

    Hero Component Initialization

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import "./style.css"
 
 export default function App() {
     return (
         <div>
-            <Navbar/>
+            <Navbar />
+            <Hero />
         </div>
     )
 }
---
+++ airbnb-clone/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Grid from "../images/photo-grid.png"
+import "../style.css"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src={Grid} alt="" className="hero--photo"></img>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
