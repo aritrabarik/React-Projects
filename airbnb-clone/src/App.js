aac53e91cfecd58b0002058523f2532db60ba41f
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-20 12:42:32 +0530

    Deleted Card Component

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./components/Card";
+//import Card from "./components/Card";
 //import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import "./style.css"
@@ -8,14 +8,6 @@ export default function App() {
     return (
         <div>
             <Navbar />
-            <Card
-                img = "katie-zaferes.png"
-                rating = "5.0"
-                reviewCount = {6}
-                country = "USA"
-                title = "Life Lessons with Katie Zaferes"
-                price = {136}
-            />
         </div>
     )
 }
