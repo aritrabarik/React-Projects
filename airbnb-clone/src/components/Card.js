86d1ff3a6acf138ca1c9d5a009054de66b5e030a
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-21 19:09:10 +0530

    Passing props to card components

--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -2,18 +2,18 @@ import React from "react";
 import katie from "../images/katie-zaferes.png"
 import star from "../images/star.png"
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
             <img src={katie} alt="Katie-Zaferes" className="card--image"></img>
             <div className="card--stats">
                 <img src={star} alt="star" className="card--star"></img>
-                <span>5.0</span>
-                <span className="grey">(6) • </span>
-                <span className="grey">USA</span>
+                <span>{props.rating}</span>
+                <span className="grey">({props.reviewCount}) • </span>
+                <span className="grey">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
             </div>
     )
 }
