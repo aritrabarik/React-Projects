13921cc8fbb22784f7ad176029892e848f883a94
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-23 15:39:26 +0530

    Map experience data into components

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,19 +3,27 @@ import Card from "./components/Card";
 //import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import "./style.css"
+import data from "./data"
 
 export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                country={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    }
+)
+
     return (
         <div>
             <Navbar />
-            <Card
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            {cards}
         </div>
     )
 }
