81f0da253b4962b73111cceda67f415bf66da78e
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-23 19:28:28 +0530

    Updated src path airbnb-logo.png

--- airbnb-clone/src/components/Navbar.js
+++ airbnb-clone/src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Logo from "../images/airbnb-logo.png"
 import "../style.css"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src={Logo} alt="airbnb-logo" className="nav--logo"></img>
+            <img src="../images/airbnb-logo.png" alt="airbnb-logo" className="nav--logo"></img>
         </nav>
     )
 }
