4f720ee270891b100fa5af67ddcb9a2171111d9d
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-24 13:06:16 +0530

    Passing object as props

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -10,14 +10,7 @@ export default function App() {
         return (
             <Card
                 key={item.id}
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                country={item.location}
-                title={item.title}
-                price={item.price}
-                openSpots={item.openSpots}
-                location={item.location}
+                item={item}
             />
         )
     })
